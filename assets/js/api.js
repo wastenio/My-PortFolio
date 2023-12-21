3b89be83e5d1733bf0278f7a3a6887f1c35c8d56
wastenio silva <wastenio.silva@gmail.com>
2023-12-21 14:31:13 -0300

    Atualização de dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 //função para solicitações dos dados de perfil de uma API do github e converte em um JSON
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/wastenio/js-developer-portfolio-DIO/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/wastenio/My-PortFolio/main/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
